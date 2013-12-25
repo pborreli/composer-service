b9d59f104ada7fbb1ecbb2227c89649653e64056
Pascal Borreli <pascal@borreli.com>
2013-12-25 14:02:57 +0000

    Progression

--- src/Ayaline/Bundle/ComposerBundle/Consumer/UploadComposerConsumer.php
+++ src/Ayaline/Bundle/ComposerBundle/Consumer/UploadComposerConsumer.php
@@ -4,8 +4,10 @@ namespace Ayaline\Bundle\ComposerBundle\Consumer;
 
 use Composer\Json\JsonFile;
 use Composer\Json\JsonValidationException;
+use SensioLabs\Security\SecurityChecker;
 use Sonata\NotificationBundle\Consumer\ConsumerEvent;
 use Sonata\NotificationBundle\Consumer\ConsumerInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
@@ -34,10 +36,11 @@ class UploadComposerConsumer implements ConsumerInterface
     {
         $pusher = $this->container->get('lopi_pusher.pusher');
         $fs = new Filesystem();
-
         $message = $event->getMessage();
         $body = $message->getValue('body');
         $channelName = $message->getValue('channelName');
+
+        $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Starting async job'));
 
         $composers_tmp_path = $this->container->getParameter('composers_tmp_path', '/dev/shm/composer/');
         $composers_tmp_path = rtrim($composers_tmp_path, '/').'/';
@@ -47,6 +50,8 @@ class UploadComposerConsumer implements ConsumerInterface
         $fs->mkdir($path);
         $fs->dumpFile($path.'/composer.json', $body);
 
+        $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Validating composer.json'));
+
         try {
             $jsonFile = new JsonFile($path.'/composer.json');
             $jsonFile->validateSchema(JsonFile::LAX_SCHEMA);
@@ -54,37 +59,71 @@ class UploadComposerConsumer implements ConsumerInterface
             $from = array($path);
             $to   = array('');
             $message = str_replace($from, $to, $exception->getMessage());
-            $pusher->trigger($channelName, 'error', array('message' => nl2br($message)));
+            $pusher->trigger($channelName, 'consumer:error', array('message' => nl2br($message)));
             return 1;
         }
 
-        $pusher->trigger($channelName, 'notice', array('message' => 'Updating...'));
+        $pusher->trigger($channelName, 'consumer:new-step', array('message' => './composer update'));
 
         $process = new Process('hhvm /usr/local/bin/composer update --no-scripts --prefer-dist --no-progress --no-dev');
         $process->setWorkingDirectory($path);
         $process->setTimeout(300);
-        $process->run();
+
+        $callback = function ($type, $data) use (&$output) {
+            if ('' == $data = trim($data)) {
+                return;
+            }
+            if ($type == 'err') {
+                $output .= $data.PHP_EOL;
+            } else {
+                $output = $data.PHP_EOL;
+            }
+        };
+
+        $output = null;
+        try {
+            $process->run($callback);
+        }catch (\Exception $e) {
+            $pusher->trigger($channelName, 'consumer:step-error', array('message' => 'HHVM composer failed'));
+        }
 
         $requirements = 'Your requirements could not be resolved to an installable set of packages.';
 
-        if (!$process->isSuccessful() && false !== strpos($process->getOutput(), $requirements)) {
+        if (!$process->isSuccessful() || false !== strpos($output, $requirements) || false !== strpos($output, 'HipHop Fatal error')) {
 
-            $pusher->trigger($channelName, 'notice', array('message' => 'Restarting...'));
+            $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Restarting...'));
 
             $process = new Process('/usr/local/bin/composer update --no-scripts --prefer-dist --no-progress --no-dev');
             $process->setWorkingDirectory($path);
             $process->setTimeout(300);
-            $process->run();
+            $output = null;
+            $process->run($callback);
         }
 
         if (!$process->isSuccessful()) {
-            $pusher->trigger($channelName, 'error', array('message' => $process->getOutput()));
+            $pusher->trigger($channelName, 'consumer:error', array('message' => nl2br($output)));
+            $pusher->trigger($channelName, 'consumer:step-error', array('message' => 'Composer failed'));
             return 1;
         }
 
         if (!is_dir($path.'/vendor') || !is_file($path.'/composer.lock')) {
-            $pusher->trigger($channelName, 'error', array('message' => 'Fatal error during composer update'));
+            $pusher->trigger($channelName, 'consumer:step-error', array('message' => 'Fatal error during composer update'));
             return 1;
+        }
+
+        $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Checking vulnerability'));
+        $checker = new SecurityChecker();
+
+        try {
+            $alerts = $checker->check($path.'/composer.lock', 'json');
+        }catch(\RuntimeException $e){
+            $pusher->trigger($channelName, 'consumer:error', array('message' => $e->getMessage()));
+            return 1;
+        }
+
+        $vulnerabilityCount = $checker->getLastVulnerabilityCount();
+        if ($vulnerabilityCount > 0) {
+            $pusher->trigger($channelName, 'consumer:step-error', array('message' => 'Vulnerability found : '.$vulnerabilityCount));
         }
 
         $sha1LockFile = sha1_file($path.'/composer.lock');
@@ -92,9 +131,10 @@ class UploadComposerConsumer implements ConsumerInterface
         $rootDir = $this->container->get('kernel')->getRootDir();
         $resultPath = $rootDir.'/../web/assets/'.$sha1LockFile;
 
-        if (!is_file($resultPath.'/vendor.zip')) {
-
-            $pusher->trigger($channelName, 'notice', array('message' => 'Compressing...'));
+        if (is_file($resultPath.'/vendor.zip')) {
+            $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Serving cached vendor.zip'));
+        } else {
+            $pusher->trigger($channelName, 'consumer:new-step', array('message' => 'Compressing vendor.zip'));
 
             $fs->mkdir($resultPath);
             $process = new Process('zip -rq '.$resultPath.'/vendor.zip .');
@@ -102,10 +142,10 @@ class UploadComposerConsumer implements ConsumerInterface
             $process->run();
 
             if (!$process->isSuccessful()) {
-                $pusher->trigger($channelName, 'error', array('message' => $process->getErrorOutput()));
+                $pusher->trigger($channelName, 'consumer:error', array('message' => $process->getErrorOutput()));
             }
         }
-        $pusher->trigger($channelName, 'success', array('link' => '/assets/'.$sha1LockFile.'/vendor.zip'));
+        $pusher->trigger($channelName, 'consumer:success', array('link' => '/assets/'.$sha1LockFile.'/vendor.zip'));
 
         $fs->remove($path);
 
--- src/Ayaline/Bundle/ComposerBundle/Controller/DefaultController.php
+++ src/Ayaline/Bundle/ComposerBundle/Controller/DefaultController.php
@@ -35,12 +35,28 @@ DCB;
         if ($form->isValid()) {
 
             $data = $form->getData();
+            $temp_file = tempnam(sys_get_temp_dir(), 'composer');
+            file_put_contents($temp_file, $data['body']);
+
+            try {
+                $jsonFile = new JsonFile($temp_file);
+                $jsonFile->validateSchema(JsonFile::LAX_SCHEMA);
+                unlink($temp_file);
+            } catch (\Exception $exception) {
+                $from = array($temp_file);
+                $to   = array('composer.json');
+                $message = str_replace($from, $to, $exception->getMessage());
+                unlink($temp_file);
+
+                return new JsonResponse(array('status' => 'ko', 'message' => nl2br($message)));
+            }
+
             $this->get('sonata.notification.backend')->createAndPublish('upload.composer', array(
                 'body' => $data['body'],
                 'channelName' => $request->getSession()->get('channelName')
             ));
 
-            return new JsonResponse(array('status' => 'ok', 'message' => 'Launching...'));
+            return new JsonResponse(array('status' => 'ok'));
         }
 
         return $this->render('AyalineComposerBundle:Default:index.html.twig', array(
