059fd19e81840c7b29f4892c164650f911ddd1c4
Pascal Borreli <pascal@borreli.com>
2014-01-12 21:34:37 +0000

    Refactor Pusher triggers

--- src/Ayaline/Bundle/ComposerBundle/Consumer/Step/AbstractStep.php
+++ src/Ayaline/Bundle/ComposerBundle/Consumer/Step/AbstractStep.php
@@ -10,16 +10,34 @@ use Sonata\NotificationBundle\Consumer\ConsumerEvent;
  */
 abstract class AbstractStep implements StepInterface
 {
+    /**
+     * @var \Pusher
+     */
     protected $pusher;
 
+    /**
+     * @var Filesystem
+     */
     protected $filesystem;
 
+    /**
+     * @var string
+     */
     protected $rootDir;
 
+    /**
+     * @var string
+     */
     protected $workingTempPath;
 
+    /**
+     * @var string
+     */
     protected $path;
 
+    /**
+     * @var string
+     */
     protected $composerBinPath;
 
     /**
@@ -73,5 +91,49 @@ abstract class AbstractStep implements StepInterface
         $message = $event->getMessage();
 
         return $message->getValue('channelName');
+    }
+
+    /**
+     * Triggers a "consumer:new-step" message on Pusher.
+     *
+     * @param ConsumerEvent $event
+     * @param array $message
+     */
+    protected function triggerNewStep(ConsumerEvent $event, $message)
+    {
+        $this->pusher->trigger($this->getChannel($event), 'consumer:new-step', $message);
+    }
+
+    /**
+     * Triggers a "consumer:step-error" message on Pusher.
+     *
+     * @param ConsumerEvent $event
+     * @param array $message
+     */
+    protected function triggerStepError(ConsumerEvent $event, $message)
+    {
+        $this->pusher->trigger($this->getChannel($event), 'consumer:step-error', $message);
+    }
+
+    /**
+     * Triggers a "consumer:error" message on Pusher.
+     *
+     * @param ConsumerEvent $event
+     * @param array $message
+     */
+    protected function triggerError(ConsumerEvent $event, $message)
+    {
+        $this->pusher->trigger($this->getChannel($event), 'consumer:error', $message);
+    }
+
+    /**
+     * Triggers a "consumer:success" message on Pusher.
+     *
+     * @param ConsumerEvent $event
+     * @param array $message
+     */
+    protected function triggerSuccess(ConsumerEvent $event, $message)
+    {
+        $this->pusher->trigger($this->getChannel($event), 'consumer:success', $message);
     }
 }
